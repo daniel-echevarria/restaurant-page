ffd845a46e469e4773fe56843a936cdf00a281de
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-06-13 17:48:03 +0200

    Make nav logic DRYer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,16 +3,24 @@ import loadWelcome from './welcome.js'
 import loadMenu from './menu.js'
 
 const loadContent = () => {
-
   const contentEl = document.getElementById('content')
-  const welcomeBtn = document.getElementById('welcome-btn')
-  welcomeBtn.addEventListener('click', function() {
-    contentEl.innerHTML = ''
-    contentEl.appendChild(loadWelcome())
-  })
-  const menuPage = loadMenu()
 
-  contentEl.append(menuPage)
+  const welcomePage = loadWelcome()
+  const welcomeBtns = document.querySelectorAll('.home-btn')
+  welcomeBtns.forEach(function(button) {
+    button.addEventListener('click', function() {
+    contentEl.innerHTML = ''
+    contentEl.appendChild(welcomePage)
+    })
+  })
+
+  const menuBtn = document.getElementById('menu-btn')
+  menuBtn.addEventListener('click', function() {
+    contentEl.innerHTML = ''
+    contentEl.appendChild(loadMenu())
+  })
+
+  contentEl.append(welcomePage)
   return contentEl
 }
 
